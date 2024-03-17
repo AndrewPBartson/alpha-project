926620eec4d09bfe7db827a66fafc0ce83ddbc6c
andrewpbartson <andrewpbartson@gmail.com>
2024-03-17 16:15:16 -0700

    Add bears image and Change Mode button to Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,41 +1,75 @@
 import Bears from '../images/bears.jpg'
+import Button from './Button'
+import { useSelector, useDispatch } from 'react-redux'
+import { setTheme } from '../features/theme/themeSlice'
 
 const Navbar = () => {
+  const dispatch = useDispatch()
+  const { theme } = useSelector((state) => state.theme)
+
+  const onChangeMode = () => {
+    console.log('onChangeMode:', theme)
+    dispatch(setTheme())
+  }
+
   return (
-    <div className='col-12'>
-      <header className='row d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom'>
-        <div className='col-sm-5 col-md-4 mb-2 mb-md-0 ps-3 pe-4'>
-          <a
-            href='/'
-            className='d-inline-flex link-body-emphasis text-decoration-none'
-          >
-            <img
-              className='truckLogo'
-              alt='Mama bear and her cubs walking'
-              src={Bears}
-              width='202'
-              height='50'
-            />
+    <nav
+      id='nav_bar'
+      data-bs-theme={theme}
+      className='navbar navbar-expand-md bg-body-tertiary'
+    >
+      <div className='container-fluid'>
+        <a className='navbar-brand' href='#'>
+          <img
+            className='family of bears'
+            alt='Mama bear and her cubs walking'
+            src={Bears}
+            width='202'
+            height='50'
+          />
+        </a>
+        <div className='pb-1'>
+          <a className='navbar-brand' href='#'>
+            Code Warrior
           </a>
         </div>
-        <a className='col-sm-3 col-md-4 navbar-brand' href='/'>
-          <h3>Code Warrior</h3>
-        </a>
-
-        <div className='col-sm-4 col-md-3 text-end'>
-          <ul className='nav navbar-nav row'>
-            <li className='col-1210'>
-              <a
-                className='link-underline-light link-opacity-50-hover'
-                href='/articles'
-              >
-                Articles
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarSupportedContent'
+          aria-controls='navbarSupportedContent'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+          <ul className='navbar-nav me-auto mb-lg-0'>
+            <li className='nav-item' onClick={onChangeMode}>
+              <a className='nav-link' href='#'>
+                <Button title='Change Mode' />
               </a>
             </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='#'>
+                <Button title='Test' />
+              </a>
+            </li>
+            {/* <li className='nav-item'>
+              <a className='nav-link' href='#'>
+                <Button title='Test' />
+              </a>
+            </li> */}
           </ul>
+          <form className='d-flex' role='search'>
+            <button className='btn btn-outline-success' type='submit'>
+              Login
+            </button>
+          </form>
         </div>
-      </header>
-    </div>
+      </div>
+    </nav>
   )
 }
 
